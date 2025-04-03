3d02f3974cf70cd2f5c2f274286a94ec99fcee04
David <david.andres.leon@iaac.net>
2025-04-03 14:28:07 +0200

    upload files

---
+++ qodo/assignment01.py
@@ -0,0 +1,8 @@
+import Rhino.Geometry as rg #type:ignore
+import math
+
+# DECLARE INPUT VARIABLES OF PYTHON COMPONENT
+x_points = cast(int, x_points)  # type: ignore
+y_points = cast(int, y_points)  # type: ignore
+
+#START CONDING HERE 
